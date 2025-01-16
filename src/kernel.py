26f6961b34d3a907b3db65d80f283547d0f3daa6
BrownianNotion <andrewwusyd@gmail.com>
2025-01-16 21:58:24 +0000

    Gaussian Kernel

--- src/kernel.py
+++ src/kernel.py
@@ -2,20 +2,37 @@ import numpy as np
 from numpy.typing import ArrayLike
 
 
+# For now, vectorise and assume 1d input x
 class Kernel:
-    def __init__(self, params: dict, kernel_function):
-        self.__params = params
-        self.__k = kernel_function
+    def __init__(self, params: dict):
+        self.params = params
+    
+    def kernel_function(x1, x2):
+        pass
 
     def evaluate(self, x1, x2):
-        return self.__k(x1, x2)
+        return self.kernel_function(x1, x2)
 
 
 class IdentityKernel(Kernel):
     def __init__(self, scale=1.0):
         params = {"scale": scale}
-        kernel_function = lambda x1, x2: scale * np.eye(len(x1))
-        super().__init__(params, kernel_function)
+        super().__init__(params)
 
-    def evaluate(self, x1, x2):
-        return super().evaluate(x1, x2)
+    def kernel_function(self, x1, x2):
+        return self.params["scale"] * np.eye(len(x1)) 
+
+class GaussianKernel(Kernel):
+    def __init__(self, amplitude, length_scale):
+        params = {
+            "sigma": amplitude,
+            "l": length_scale
+        }
+        super().__init__(params)
+    
+    def kernel_function(self, x1, x2):
+        x1 = np.expand_dims(x1, axis=0)
+        x2 = np.expand_dims(x2, axis=1)
+        s, l = self.params["sigma"], self.params["l"]
+        return s * np.exp(-0.5 * (x1 - x2)**2/ l**2) 
+
--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -2,6 +2,7 @@ import numpy as np
 import numpy.testing as npt
 
 from src.kernel import IdentityKernel
+from src.kernel import GaussianKernel
 
 np.random.seed(42)
 
@@ -12,5 +13,19 @@ def test_identity_kernel():
     x1 = np.random.rand(N, 1)
     x2 = np.random.rand(N, 1)
 
-    id_kernel = IdentityKernel(scale=scale)
-    npt.assert_allclose(id_kernel.evaluate(x1, x2), scale * np.eye(N))
+    kernel = IdentityKernel(scale=scale)
+    npt.assert_allclose(kernel.evaluate(x1, x2), scale * np.eye(N))
+
+def test_gaussian_kernel():
+    s = .5
+    l = 2.
+    
+    kernel = GaussianKernel(s, l)
+    
+    x1 = np.array([1.0, 0.])
+    
+    K_desired = np.array([
+        [0.5, 0.5 * np.exp(-0.125)],
+        [0.5 * np.exp(-0.125), 0.5]
+    ])
+    npt.assert_allclose(kernel.evaluate(x1, x1), K_desired)
